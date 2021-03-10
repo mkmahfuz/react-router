652d2d6edf65d5e3fb1409227a497ed174157a41
mahfuz <mdmahfuzbd@gmail.com>
2021-03-10 06:18:45 +0600

    components home,teams,teamdetails created

---
+++ src/components/TeamDetails/TeamDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TeamDetails = () => {
+    return (
+        <div>
+            <h1>This is TeamDetails</h1>
+        </div>
+    );
+};
+
+export default TeamDetails;
